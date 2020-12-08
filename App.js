4db16d03272d9bffa6bac14063bf19fc9734f359
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-12-08 17:55:40 +0800

    Nevigation implemented with some minner problems

--- App.js
+++ App.js
@@ -1,36 +1,63 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
+import {Button, View, StatusBar} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const Drawer = createDrawerNavigator();
+const HomeStack = createStackNavigator();
+const NotificationStack = createStackNavigator();
+
+function HomeScreen({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Button
+                onPress={() => navigation.navigate('Notifications')}
+                title="Go to notifications"
+            />
+        </View>
+    );
+}
+
+function NotificationsScreen({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Button onPress={() => navigation.goBack()} title="Go back home"/>
+        </View>
+    );
+}
+
+const HomeStackScreen = ({navigation}) => (
+    <HomeStack.Navigator screenOptions={{
+        headerStyle: {backgroundColor: '#009387'},
+        headerTintColor: '#fff',
+        headerTitleStyle: {fontWeight: 'normal'},
+    }}>
+        <HomeStack.Screen name="Home" component={HomeScreen} options={{title: 'Home'}}/>
+    </HomeStack.Navigator>
+)
+
+const NotificationStackScreen = ({navigation}) => (
+    <NotificationStack.Navigator screenOptions={{
+        headerStyle: {backgroundColor: '#009387'},
+        headerTintColor: '#fff',
+        headerTitleStyle: {fontWeight: 'normal'},
+    }}>
+        <NotificationStack.Screen name="Notification" component={NotificationsScreen} options={{title: 'Notification'}}/>
+    </NotificationStack.Navigator>
+);
+
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+    StatusBar.setBarStyle('light-content', true);
+    StatusBar.setBackgroundColor('#009387');
+
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home">
+                <Drawer.Screen name="Home" component={HomeStackScreen}/>
+                <Drawer.Screen name="Notifications" component={NotificationStackScreen}/>
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
 }
