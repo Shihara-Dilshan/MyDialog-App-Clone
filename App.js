8eda8a845f36995ae11a885f47cd76264432b19b
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-12-08 18:37:57 +0800

    fixed bugs

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {Button, View, StatusBar} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Drawer = createDrawerNavigator();
 const HomeStack = createStackNavigator();
@@ -23,6 +24,7 @@ function NotificationsScreen({navigation}) {
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Button onPress={() => navigation.goBack()} title="Go back home"/>
+            
         </View>
     );
 }
@@ -31,11 +33,11 @@ const HomeStackScreen = ({navigation}) => (
     <HomeStack.Navigator screenOptions={{
         headerStyle: {backgroundColor: '#009387'},
         headerTintColor: '#fff',
-        headerTitleStyle: {fontWeight: 'normal'},
+        headerTitleStyle: {fontWeight: 'normal'}
     }}>
-        <HomeStack.Screen name="Home" component={HomeScreen} options={{title: 'Home'}}/>
+        <HomeStack.Screen name="Home" component={HomeScreen} options={{title: 'Home', headerLeft: () => (<Icon.Button name="ios-menu" size={25} backgroundColor="#009387" onPress={ () => navigation.openDrawer()}></Icon.Button>) }} />
     </HomeStack.Navigator>
-)
+);
 
 const NotificationStackScreen = ({navigation}) => (
     <NotificationStack.Navigator screenOptions={{
@@ -43,7 +45,7 @@ const NotificationStackScreen = ({navigation}) => (
         headerTintColor: '#fff',
         headerTitleStyle: {fontWeight: 'normal'},
     }}>
-        <NotificationStack.Screen name="Notification" component={NotificationsScreen} options={{title: 'Notification'}}/>
+        <NotificationStack.Screen name="Notification" component={NotificationsScreen} options={{title: 'Notification', headerLeft: () => (<Icon.Button name="ios-menu" size={25} backgroundColor="#009387" onPress={ () => navigation.openDrawer()}></Icon.Button>) }} />
     </NotificationStack.Navigator>
 );
 
