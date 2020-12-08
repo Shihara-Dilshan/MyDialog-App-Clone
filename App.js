3d184fff8f70eba8c91659f259348d4cc4932870
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-12-08 21:13:28 +0800

    stable 0.0.1

--- App.js
+++ App.js
@@ -1,33 +1,17 @@
 import * as React from 'react';
-import {Button, View, StatusBar} from 'react-native';
+import {StatusBar} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+import HomeScreen from "./Components/MainTabs/Home";
+import NotificationsScreen from "./Components/MainTabs/Notification";
+
 const Drawer = createDrawerNavigator();
 const HomeStack = createStackNavigator();
 const NotificationStack = createStackNavigator();
 
-function HomeScreen({navigation}) {
-    return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Button
-                onPress={() => navigation.navigate('Notifications')}
-                title="Go to notifications"
-            />
-        </View>
-    );
-}
-
-function NotificationsScreen({navigation}) {
-    return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Button onPress={() => navigation.goBack()} title="Go back home"/>
-            
-        </View>
-    );
-}
 
 const HomeStackScreen = ({navigation}) => (
     <HomeStack.Navigator screenOptions={{
@@ -35,7 +19,7 @@ const HomeStackScreen = ({navigation}) => (
         headerTintColor: '#fff',
         headerTitleStyle: {fontWeight: 'normal'}
     }}>
-        <HomeStack.Screen name="Home" component={HomeScreen} options={{title: 'Home', headerLeft: () => (<Icon.Button name="ios-menu" size={25} backgroundColor="#009387" onPress={ () => navigation.openDrawer()}></Icon.Button>) }} />
+        <HomeStack.Screen name="Home" component={HomeScreen} options={{title: 'Home', headerLeft: () => (<Icon.Button name="ios-menu" size={25} backgroundColor="#009387" onPress={() => navigation.openDrawer()}/>) }} />
     </HomeStack.Navigator>
 );
 
@@ -63,3 +47,4 @@ export default function App() {
         </NavigationContainer>
     );
 }
+
